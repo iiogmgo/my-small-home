b289b559f8cad385bbfde3187e8cbb9c7539548b
aeyeong <iiogmgo@ridi.com>
2019-01-03 22:14:00 +0900

    Separate resources

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, {
 import './App.css';
 import Gallery from './Gallery';
 import Header from './Header';
+import { THUMBNAIL_IMAGES } from './Resource';
 
 class App extends Component {
   renderGallery() {
@@ -22,20 +23,20 @@ class App extends Component {
   }
 }
 
-const THUMBNAIL_IMAGES = [
-  {src: 'images/14.jpeg', orientation: 'square'},
-  {src: 'images/2.jpeg', orientation: 'square'},
-  {src: 'images/12.jpeg', orientation: 'landscape'},
-  {src: 'images/5.jpeg', orientation: 'landscape'},
-  {src: 'images/4.jpeg', orientation: 'square'},
-  {src: 'images/6.jpeg', orientation: 'landscape'},
-  {src: 'images/7.jpeg', orientation: 'square'},
-  {src: 'images/8.jpeg', orientation: 'landscape'},
-  {src: 'images/9.jpeg', orientation: 'landscape'},
-  {src: 'images/10.jpeg', orientation: 'landscape'},
-  {src: 'images/1.jpeg', orientation: 'square'},
-  {src: 'images/3.jpeg', orientation: 'landscape'},
-  {src: 'images/13.jpeg', orientation: 'landscape'},
-];
+// const THUMBNAIL_IMAGES = [
+//   {src: 'images/14.jpeg', orientation: 'square'},
+//   {src: 'images/2.jpeg', orientation: 'square'},
+//   {src: 'images/12.jpeg', orientation: 'landscape'},
+//   {src: 'images/5.jpeg', orientation: 'landscape'},
+//   {src: 'images/4.jpeg', orientation: 'landscape'},
+//   {src: 'images/6.jpeg', orientation: 'landscape'},
+//   {src: 'images/7.jpeg', orientation: 'square'},
+//   {src: 'images/8.jpeg', orientation: 'landscape'},
+//   {src: 'images/9.jpeg', orientation: 'landscape'},
+//   {src: 'images/10.jpeg', orientation: 'landscape'},
+//   {src: 'images/3.jpeg', orientation: 'landscape'},
+//   {src: 'images/13.jpeg', orientation: 'landscape'},
+//   {src: 'images/1.jpeg', orientation: 'landscape'},
+// ];
 
 export default App;
---
+++ src/Resource.js
@@ -0,0 +1,61 @@
+const THUMBNAIL_IMAGES = [{
+    src: 'images/14.jpeg',
+    orientation: 'square',
+    caption: '바다가 괜히 예뻤다.'
+  },
+  {
+    src: 'images/2.jpeg',
+    orientation: 'square',
+    caption: '시골이 낯선 시절에'
+  },
+  {
+    src: 'images/12.jpeg',
+    orientation: 'landscape',
+    caption: '요양을 했다, 고기로'
+  },
+  {
+    src: 'images/5.jpeg',
+    orientation: 'landscape'
+  },
+  {
+    src: 'images/4.jpeg',
+    orientation: 'landscape'
+  },
+  {
+    src: 'images/6.jpeg',
+    orientation: 'landscape'
+  },
+  {
+    src: 'images/7.jpeg',
+    orientation: 'square'
+  },
+  {
+    src: 'images/8.jpeg',
+    orientation: 'landscape'
+  },
+  {
+    src: 'images/9.jpeg',
+    orientation: 'landscape'
+  },
+  {
+    src: 'images/10.jpeg',
+    orientation: 'landscape'
+  },
+  {
+    src: 'images/3.jpeg',
+    orientation: 'landscape'
+  },
+  {
+    src: 'images/13.jpeg',
+    orientation: 'landscape'
+  },
+  {
+    src: 'images/1.jpeg',
+    orientation: 'landscape',
+    caption: '너 크면 바보된다'
+  },
+];
+
+export {
+  THUMBNAIL_IMAGES
+};
